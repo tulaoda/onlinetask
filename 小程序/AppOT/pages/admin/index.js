3f6f76ecb778ca35f04352d2216d24151256cb1b
tulaoda <2513691817@qq.com>
2018-11-04 15:40:45 +0800

    小程序ui 完成

--- 小程序/AppOT/pages/admin/index.js
+++ 小程序/AppOT/pages/admin/index.js
@@ -1,11 +1,10 @@
-// pages/type2/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    current: 2
+
   },
 
   /**
--- 小程序/AppOT/pages/index/index.js
+++ 小程序/AppOT/pages/index/index.js
@@ -1,4 +1,4 @@
-const SERVER = require('../../utils/server.js');
+const SERVER = require('../../utils/server.js')
 Page({
   data: {
     imgUrls: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3864886031,2376753105&fm=11&gp=0.jpg',
@@ -10,17 +10,17 @@ Page({
     duration: 1000,
     grids: [0, 1, 2, 3],
     msgList: [{
-        url: "url",
-        title: "消息：用户蜡笔小新完成任务，获得佣金三块"
-      },
-      {
-        url: "url",
-        title: "消息：悦如公寓三周年生日趴邀你免费吃喝欢唱"
-      },
-      {
-        url: "url",
-        title: "消息：你想和一群有志青年一起过生日嘛？"
-      }
+      url: 'url',
+      title: '消息：用户蜡笔小新完成任务，获得佣金三块'
+    },
+    {
+      url: 'url',
+      title: '消息：悦如公寓三周年生日趴邀你免费吃喝欢唱'
+    },
+    {
+      url: 'url',
+      title: '消息：你想和一群有志青年一起过生日嘛？'
+    }
     ]
 
   },
@@ -28,20 +28,15 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(e) {
+    // 小程序版本更新提示
 
-
-
-    //小程序版本更新提示
-
-
-    //检查是否存在新版本
+    // 检查是否存在新版本
     wx.getUpdateManager().onCheckForUpdate(function(res) {
       // 请求完新版本信息的回调
-      console.log("是否有新版本：" + res.hasUpdate);
-      if (res.hasUpdate) { //如果有新版本
-
+      console.log('是否有新版本：' + res.hasUpdate)
+      if (res.hasUpdate) { // 如果有新版本
         // 小程序有新版本，会主动触发下载操作（无需开发者触发）
-        wx.getUpdateManager().onUpdateReady(function() { //当新版本下载完成，会进行回调
+        wx.getUpdateManager().onUpdateReady(function() { // 当新版本下载完成，会进行回调
           wx.showModal({
             title: '更新提示',
             content: '新版本已经准备好，单击确定重启应用',
@@ -49,41 +44,40 @@ Page({
             success: function(res) {
               if (res.confirm) {
                 // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-                wx.getUpdateManager().applyUpdate();
+                wx.getUpdateManager().applyUpdate()
               }
             }
           })
-
         })
 
         // 小程序有新版本，会主动触发下载操作（无需开发者触发）
-        wx.getUpdateManager().onUpdateFailed(function() { //当新版本下载失败，会进行回调
+        wx.getUpdateManager().onUpdateFailed(function() { // 当新版本下载失败，会进行回调
           wx.showModal({
             title: '提示',
             content: '检查到有新版本，但下载失败，请检查网络设置',
-            showCancel: false,
+            showCancel: false
           })
         })
       }
-    });
+    })
 
 
-    var that = this;
+    var that = this
     //轮播图
     SERVER.getJSON('/banner/listBanner', {}, function(res) {
-      let imgUrls = [];
+      const imgUrls = []
       res.data.content.map(item => {
         imgUrls.push(item.imgUrl)
-      });
-      that.setData({
-        imgUrls: imgUrls,
       })
-    });
+      that.setData({
+        imgUrls: imgUrls
+      })
+    })
 
   },
   banner_detail: function() {
     wx.navigateTo({
-      url: '../banner_detail/index',
+      url: '../banner_detail/index'
     })
   },
   developing: function() {
