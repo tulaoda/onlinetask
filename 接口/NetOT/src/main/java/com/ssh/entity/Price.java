0c474adced4f00111d54d5a6923bc801016d8d63
tulaoda-PC\tulaoda <2513691817@qq.com>
2018-11-05 22:16:16 +0800

    后台开始开发

--- 接口/NetOT/src/main/java/com/ssh/entity/Price.java
+++ 接口/NetOT/src/main/java/com/ssh/entity/Price.java
@@ -8,6 +8,7 @@ import javax.persistence.Table;
 @Table(name = "t_price")
 public class Price {
     @Id
+
     private int id;
     private double price;
 
--- 接口/NetOT/src/main/java/com/ssh/entity/Task.java
+++ 接口/NetOT/src/main/java/com/ssh/entity/Task.java
@@ -2,7 +2,6 @@ package com.ssh.entity;
 
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,22 +10,43 @@ import java.util.List;
 @Entity
 @Table(name = "t_task")
 public class Task {
+
+    //    @GenericGenerator(name = "systemUUID", strategy = "uuid")
+//    @GeneratedValue(generator = "systemUUID")
     @Id
-    @GenericGenerator(name = "systemUUID", strategy = "uuid")
-    @GeneratedValue(generator = "systemUUID")
-    private String taskId;//任务id
+    @GeneratedValue
+    @Column(name = "taskId")
+    private Long taskId;//任务id
+
+    @Column(name = "name")
     private String name;//任务名称
+
+    @Column(name = "commission")
     private double commission;//佣金
+
+    @Column(name = "article")
     private String article;//文案
+
+    @Column(name = "createTime")
     private String createTime;//创建时间
+
+    @Column(name = "totalNo")
     private int totalNo;//任务总量
+
+    @Column(name = "remainNo")
     private int remainNo;//剩余数量
-    private int State;//任务状态  1 任务进行中  2 任务已满员  3 任务人为结束
-    @OneToMany(targetEntity = Imgs.class, cascade = CascadeType.ALL)
+
+    @Column(name = "state")
+    private int state;//任务状态  1 任务进行中  2 任务已满员  3 任务人为结束
+
+
+    @OneToMany(targetEntity = ImgsTask.class, cascade = CascadeType.ALL)
     @Fetch(FetchMode.JOIN)
     //updatable=false很关键，如果没有它，在级联删除的时候就会报错(反转的问题)
     @JoinColumn(name = "foreignId", updatable = false)
-    private List<Imgs> taskImgs = new ArrayList<Imgs>();//图片
+    private List<ImgsTask> taskImgs = new ArrayList<ImgsTask>();//图片
+
+
 
     @OneToMany(targetEntity = TaskOrder.class, cascade = CascadeType.ALL)
     @Fetch(FetchMode.JOIN)
@@ -37,23 +57,23 @@ public class Task {
     public Task() {
     }
 
-    public Task(String name, double commission, String article, String createTime, int totalNo, int remainNo, int state, List<Imgs> taskImgs, List<TaskOrder> taskOrders) {
+    public Task(String name, double commission, String article, String createTime, int totalNo, int remainNo, int state, List<ImgsTask> taskImgs, List<TaskOrder> taskOrders) {
         this.name = name;
         this.commission = commission;
         this.article = article;
         this.createTime = createTime;
         this.totalNo = totalNo;
         this.remainNo = remainNo;
-        State = state;
+        state = state;
         this.taskImgs = taskImgs;
         this.taskOrders = taskOrders;
     }
 
-    public String getTaskId() {
+    public Long getTaskId() {
         return taskId;
     }
 
-    public void setTaskId(String taskId) {
+    public void setTaskId(Long taskId) {
         this.taskId = taskId;
     }
 
@@ -106,18 +126,18 @@ public class Task {
     }
 
     public int getState() {
-        return State;
+        return state;
     }
 
     public void setState(int state) {
-        State = state;
+        state = state;
     }
 
-    public List<Imgs> getTaskImgs() {
+    public List<ImgsTask> getTaskImgs() {
         return taskImgs;
     }
 
-    public void setTaskImgs(List<Imgs> taskImgs) {
+    public void setTaskImgs(List<ImgsTask> taskImgs) {
         this.taskImgs = taskImgs;
     }
 
